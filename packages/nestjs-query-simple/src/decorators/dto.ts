df55870080cf8da550f8e2cd5fa7a89a60ac9f63
choresh <raanan34@gmail.com>
2024-01-28 15:58:22 +0200

    Update code docs.

--- packages/nestjs-query-simple/src/decorators/dto.ts
+++ packages/nestjs-query-simple/src/decorators/dto.ts
@@ -1,10 +1,16 @@
 import { InputType } from '@nestjs/graphql'
 
+/**
+ * Possible types of a DTO.
+ * * input: the DTO contains data for creation of new MongoDB document.
+ * * update: the DTO contains data for updating of exists MongoDB document.
+ */
 export type DtoType = 'input' | 'update'
 
 /**
  * Decorator that marks a class as a GraphQL input/update DTO type.
  * @param {string} name - The dto name, as exposed by GraphQL ('Input' or 'Update' suffix will be added automaticly, according the 'type' parameter).
+ * @param {DtoType} type - The dto type (input: the DTO contains data for creation of new MongoDB document, update: the DTO contains data for updating of exists MongoDB document).
  * @returns {ClassDecorator} - The decorator function.
  */
 export function Dto (name: string, type: DtoType): ClassDecorator {
